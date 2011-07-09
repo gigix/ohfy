a8e3b5ddb67dab7bef4a84271136029f88c513d5
Jeff Xiong <gigix1980@gmail.com>
2011-07-09 09:44:04 +0800

    show more information if push to sina failed

--- rails_root/script/share.rb
+++ rails_root/script/share.rb
@@ -2,6 +2,8 @@ User.find(:all).each do |user|
   begin 
     user.share_to_sina(Date.today.to_s(:db))
   rescue => e
+    puts "Error happens when dealing user ##{user.id} :"
+    puts e.message
     puts e.backtrace.join("\n")
   end
 end
