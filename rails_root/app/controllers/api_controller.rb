e331f9affdcb1a14a164adc14ee71f7895e89f0b
Jeff Xiong <gigix1980@gmail.com>
2011-12-04 18:42:22 +0800

    server side logic of updating todo item

--- rails_root/app/controllers/api_controller.rb
+++ rails_root/app/controllers/api_controller.rb
@@ -12,7 +12,19 @@ class ApiController < ApplicationController
   
   def todos
     sign_in_token = request.headers[SIGN_IN_TOKEN_NAME]
-    execution = User.find_by_sign_in_token(sign_in_token).execution_on_today
-    render :text => execution.to_json(:only => :id, :methods => :todo_items)
+    raise "Invalid access" if sign_in_token.strip.blank?
+    user = User.find_by_sign_in_token(sign_in_token)
+    
+    if request.get?
+      execution = User.find_by_sign_in_token(sign_in_token).execution_on_today
+      render :text => execution.to_json(:only => :id, :methods => :todo_items)
+    else
+      execution = User.find_by_sign_in_token(sign_in_token).executions.find(params[:execution_id])
+      habit = execution.habits.find(params[:habit_id])
+      done = (params[:done] == "true")
+      
+      done ? execution.act!(habit) : execution.deact!(habit)
+      render :text => "Success"
+    end
   end    
 end
--- rails_root/spec/controllers/api_controller_spec.rb
+++ rails_root/spec/controllers/api_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ApiController do
   before(:each) do
-    @user = create_test_user
+    @user = create_user_with_plans
   end
   
   describe :sign_in do
@@ -15,27 +15,43 @@ describe ApiController do
     
     it "does not return token for invalid sign in" do
       post :sign_in, :email => @user.email, :password => "not.this.password"
-      response.header["sign_in_token"].should be_nil
+      response.header[ApiController::SIGN_IN_TOKEN_NAME].should be_nil
     end
     
     it "does not return token for email not exist" do
       post :sign_in, :email => "does.not.exist@user.com", :password => "not.this.password"
-      response.header["sign_in_token"].should be_nil
+      response.header[ApiController::SIGN_IN_TOKEN_NAME].should be_nil
     end
   end
   
   describe :todos do
     before(:each) do
       post :sign_in, :email => @user.email, :password => @user.password
-      sign_in_token = response.header["sign_in_token"]      
-      request.headers["sign_in_token"] = sign_in_token
+      sign_in_token = response.header[ApiController::SIGN_IN_TOKEN_NAME]
+      request.env[ApiController::SIGN_IN_TOKEN_NAME] = sign_in_token
     end
     
     it "returns activities of today" do
       get :todos
-      response.body.should be_include("Android")
+      response.body.should be_include("Swimming")
+      response.body.should be_include("Drawing")
     end
     
-    it "updates execution status"
+    it "updates execution status" do
+      execution = @user.execution_on_today
+      habit = execution.habits.first
+      execution.should_not be_acted(habit)
+      
+      post :todos, :execution_id => execution.id, :habit_id => habit.id, :done => "true"
+      response.should be_success
+      
+      execution.reload
+      execution.should be_acted(habit)
+    end
+    
+    it "fails without sign in token" do
+      request.env[ApiController::SIGN_IN_TOKEN_NAME] = " "
+      lambda{ get :todos }.should raise_error
+    end
   end
 end
